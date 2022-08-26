55cf0db4c4536aee9d137cbe96a5b5a63b6db38e
ajayMaharjan <ajaymaharjan758@gmail.com>
2022-08-26 17:32:11 +0545

    Add Components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Hello from './Components/Hello';
+function App() {
+  return (
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
