de0d228f0618ab145d9fff3329325874f8865f2f
ajayMaharjan <ajaymaharjan758@gmail.com>
2022-08-28 12:12:50 +0545

    todo form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-import Hello from './Components/Hello';
-import City from './Components/City';
+// import Hello from './Components/Hello';
+// import City from './Components/City';
+import Todo from './Components/Todo';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello, I am in London. It is Beautiful.</p>
-      <p>Hello, I am in New York. It is Fantastic.</p>
-      <p>Hello, I am in kathmandu. It is Majestic.</p>
-      <p>Hello, I am in Beijing. It is Movelone.</p>
-      <Hello/>
-      <City/>
+    // <div className="App">
+    //   <p>Hello, I am in London. It is Beautiful.</p>
+    //   <p>Hello, I am in New York. It is Fantastic.</p>
+    //   <p>Hello, I am in kathmandu. It is Majestic.</p>
+    //   <p>Hello, I am in Beijing. It is Movelone.</p>
+    //   <Hello/>
+    //   <City/>
+    // </div>
+
+    <div className='App'>
+      <Todo/>
     </div>
   );
 }
