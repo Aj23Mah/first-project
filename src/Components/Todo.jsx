4087b7eebdffc7f092e986f8fa7d434306c684fd
ajayMaharjan <ajaymaharjan758@gmail.com>
2022-09-01 11:30:45 +0545

    add and remove data in todo

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,45 +1,61 @@
 import React from 'react'
 import './Todo.css'
-// import { useState } from 'react';
+import { useState } from 'react';
 
  const Todo = () => {
-//     const [input, setInput] = useState([]);
-//   const [todos, setTodos] = useState([
-//     "Take a walk.",
-//     "Buy grocery.",
-//     "Read a book.",
-//     "Take a nap.",
-//   ]);
+    const [inputData, setInputData] = useState('');
+    const [todos, setTodos] = useState([
+        "Take a walk.",
+        "Buy grocery.",
+        "Read a book.",
+        "Take a nap."
+    ]);
 
-//   console.log(input);
+  console.log(inputData);
 //   console.log(todos);
 
-//   function handleSubmit() {
-//     setTodos([...todos, input]);
-//     setInput('');
-//   }
+  function handleSubmit() {
+    setTodos([...todos, inputData]);
+    setInputData('');
+  }
+  // Remove: remove all data at once.
+  function removeall() {
+    setTodos([]);
+  }
   
   return (
     <div className='todo'>
-        <div className='todoLogo'>
+        <div className='todo-logo'>
             <h1>TODO App</h1>
         </div>
         <div className='todo-form'>
             <form>
-                <input type="text" className='text' name="task" />
-                <input type="button" className='button' value="btn" />
+                <input 
+                type="text" 
+                className='input' 
+                placeholder='Add items'
+                value={inputData} 
+                onChange = {(e) => setInputData(e.target.value)} 
+                />
+                <input 
+                type="button" className='button' 
+                value="Add Todo" 
+                disabled={!inputData} 
+                onClick={handleSubmit} />
             </form>
         </div>
         <div className='todo-list'>
-            <h2>Task List</h2>
+            <h2>Task Lists</h2>
             <ul>
-                <li>Take a walk.</li>
-                <li>make a presentaton.</li>
-                <li>Cook food.</li>
-                <li>Feed the dog.</li>
+                {
+                todos.map((todo) => (
+                    <li>{todo}</li>
+                ))
+                }
             </ul>
         </div>
-        <input type="button" className='todo-removeall' value="Remove All"/>
+        <input type="button" className='todo-removeall' value="Remove All" onClick={removeall}/>
+        
     </div>
   )
 }
